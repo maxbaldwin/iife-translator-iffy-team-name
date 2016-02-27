1ac9f35662d2491c0ad051f82726c189b1a30844
Max Baldwin <maxrbaldwin@gmail.com>
2016-02-27 10:05:24 -0600

    dropping files

---
+++ latin_page.js
@@ -0,0 +1,16 @@
+  var latinLangChristmas = ["felicem", "natal", "Christi"];
+  console.log("latinLangChristmas", latinLangChristmas);
+  var engLangChristmas = ["Merry", "Christmas"];
+  console.log("engLangChristmas", engLangChristmas)
+  var latinLangNewYears = ["New", "Anno", "Felix"];
+  console.log("latinLangNewYears", latinLangNewYears);
+  var engLangNewYears = ["Happy", "New", "Years"];
+  console.log("engLangNewYears", engLangNewYears );
+  var latinLangHolidays = ["Felices", "fiestas"];
+  console.log("latinLangHolidays", latinLangHolidays );
+  var engLangHolidays = ["Happy", "Holidays"];
+  console.log("engLangHolidays", engLangHolidays );
+  var latinAnd = ["et"];
+  console.log("latinAnd", latinAnd );
+  var engAnd = ["and"];
+  console.log("engAnd", engAnd);
